d260fcdcfbe1e04a3435806df133d912b218af2b
kadam0 <k.adamowicz@tum.de>
2022-02-22 20:02:11 +0100

    mod

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="digest",
+    name="bio-digest",
     version="0.0.1",
     author="Klaudia Adamowicz",
     author_email='klaudia.adamowicz@uni-hamburg.de',
@@ -19,6 +19,5 @@ setuptools.setup(
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.8',
     py_modules=["bio-digest"],
-    package_dir={'': 'digest'},
     install_requires=[]
 )
