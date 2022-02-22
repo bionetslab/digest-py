dce769f0e63e0f1df87a82ec45a7f03e0f8538e6
kadam0 <k.adamowicz@tum.de>
2022-02-22 19:44:21 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="digest",
+    version="0.0.1",
+    author="Klaudia Adamowicz",
+    author_email='klaudia.adamowicz@uni-hamburg.de',
+    description="Disease and Gene Set and Clustering Validation Tool (DIGEST)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['digest', 'digest.evaluation', 'digest.evaluation.d_utils', 'digest.evaluation.mappers'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.8',
+    py_modules=["bio-digest"],
+    package_dir={'': 'digest'},
+    install_requires=[]
+)
