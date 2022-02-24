277e00d622c8e31793fbfb40ecd9dfe0e903d8cf
kadam0 <k.adamowicz@tum.de>
2022-02-24 11:27:43 +0100

    added to pypi

--- deploy_package.sh
+++ deploy_package.sh
@@ -3,3 +3,6 @@ python setup.py sdist
 
 # test on test pypi
 python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/biodigest-0.0.4.tar.gz
+
+# upload to pypi
+python3 -m twine upload dist/biodigest-0.0.4.tar.gz
