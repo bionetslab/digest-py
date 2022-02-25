ae9a7a6a469f5d14d12e053e8c96de31a66a74fc
kadam0 <k.adamowicz@tum.de>
2022-02-25 18:28:03 +0100

    0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biodigest",
-    version="0.0.7",
+    version="0.0.9",
     author="Klaudia Adamowicz",
     author_email='klaudia.adamowicz@uni-hamburg.de',
     url='http://pypi.python.org/pypi/biodigest/',
