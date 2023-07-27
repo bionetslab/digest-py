fdd17f3f162082f0e0984a845992e2d4465aa314
kadam0 <k.adamowicz@tum.de>
2023-07-27 08:59:01 +0200

    import fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biodigest",
-    version="0.2.8",
+    version="0.2.10",
     author="Klaudia Adamowicz",
     author_email='klaudia.adamowicz@uni-hamburg.de',
     url='http://pypi.python.org/pypi/biodigest/',
@@ -24,14 +24,14 @@ setuptools.setup(
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.7',
     install_requires=[
-        "pandas>=1.2.0",
-        "numpy>=1.20.0",
-        "scipy>=1.5.4",
-        "seaborn==0.11.2",
+        "pandas==1.5.2",
+        "numpy==1.24.3",
+        "scipy==1.8.0",
+        "seaborn==0.12.2",
         "biothings_client==0.2.6",
-        "gseapy==0.10.5",
+        "gseapy==1.0.4",
         "psutil==5.9.0",
-        "requests>=2.26.0",
-        "pycairo>=1.20.1",
+        "requests==2.28.2",
+        "pycairo==1.21.0",
     ]
 )
